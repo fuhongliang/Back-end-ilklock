3ca6609326880762eeae8197839306fb325a4934
biggraybear <1214015687@qq.com>
2019-12-27 16:32:10 +0800

    ‘s’

--- config/config.default.js
+++ config/config.default.js
@@ -101,12 +101,12 @@ module.exports = appInfo => {
 
   // 错误处理
   config.onerror = {
-    all(err, ctx) {
-      // 在此处定义针对所有响应类型的错误处理方法
-      // 注意，定义了 config.all 之后，其他错误处理方法不会再生效
-      ctx.body = { code: 1000,msg: err };
-      ctx.status = 500;
-    },
+    // all(err, ctx) {
+    //   // 在此处定义针对所有响应类型的错误处理方法
+    //   // 注意，定义了 config.all 之后，其他错误处理方法不会再生效
+    //   ctx.body = { code: 1000,msg: err };
+    //   ctx.status = 500;
+    // },
     html(err, ctx) {
       // html hander
       ctx.body = '<h3>' + err + '</h3>';
